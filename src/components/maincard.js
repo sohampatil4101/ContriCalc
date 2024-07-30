2e59328072e49db6538b2ac361e268846573a6f5
Soham Patil <patilsoham390@gmail.com>
2024-07-31 01:12:42 +0530

    started with basic cards

---
+++ src/components/maincard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../App.css';
+
+export default function Maincard() {
+  return (
+    <>
+    <div className="main-card">
+        <div className="card">
+            <input type="text" />
+            <input type="number" />
+        </div>
+    </div>
+    </>
+  )
+}
