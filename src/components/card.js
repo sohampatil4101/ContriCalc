cce74831fa94ca1bccab7473e36be45bbf571f78
Soham Patil <patilsoham390@gmail.com>
2024-08-02 00:24:49 +0530

    created calculation logic

--- src/components/card.js
+++ src/components/card.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../App.css';
 
 export default function Card(props) {
+    
+    const [data, setData] = useState({
+        text:"",
+        number:""
+    })
+    function onchange(e){
+        console.log(data.text, data.number)
+        setData({...data, [e.target.name]: e.target.value}) 
+        
+    }
+
   return (
     <>
-        <input type="text" />
-        <input type="number" />
+        <input type="text" name='text' value={data.text} onChange={onchange}/>
+        <input type="number" name='number' value={data.number} onChange={onchange}/>
         <div className="add">
             <button onClick={props.setcount}>+</button>
         </div>
